35c4980ba7ef5afd3391508faefc1761fc934e7e
Mikey Williams <manicdesigner@gmail.com>
2020-08-11 15:20:25 -0400

    start working on main code section

--- src/adv.py
+++ src/adv.py
@@ -38,11 +38,14 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-player = Player('mikey', room['outside'])
+name = input('please enter your name ')
+player = Player(name, room['outside'])
+
 # Write a loop that:
 #
 # * Prints the current room name
 print(player)
+
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
@@ -50,3 +53,5 @@ print(player)
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    choice = input(f'Please choose ')

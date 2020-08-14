6f6c587e37d35afd854e7a613cadb3190fea330d
Mikey Williams <manicdesigner@gmail.com>
2020-08-13 20:18:29 -0400

    corrected import added another function returning just the room

--- src/player.py
+++ src/player.py
@@ -1,5 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
 
 class Player:
     def __init__(self, name, current_room):
@@ -8,3 +10,6 @@ class Player:
 
     def __str__(self):
         return f'{self.name} is {self.current_room}'
+    
+    def room_current(self):
+        return f'{self.current_room}'

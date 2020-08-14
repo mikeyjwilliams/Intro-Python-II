1ff2f7636db20bb52a30fa95500a3240aa35b8da
Mikey Williams <manicdesigner@gmail.com>
2020-08-13 20:19:02 -0400

    add room function return just room name

--- src/room.py
+++ src/room.py
@@ -9,11 +9,13 @@ class Room:
         self.s_to = s_to
         self.e_to = e_to
         self.w_to = w_to
-       
 
 
     def __str__(self):
         return f'{self.name}, {self.description}'
+    
+    def room_name(self):
+        return f'{self.name}'
 
     def room_description(self):
         return f'{self.description}'

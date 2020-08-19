1afeddd1410acc334bbe0bff51ff20515cdbdecb
Mikey Williams <manicdesigner@gmail.com>
2020-08-18 23:02:05 -0400

    add on_take method to Item class

--- src/item.py
+++ src/item.py
@@ -4,13 +4,17 @@ base class for specialized item types to be declared later.
 
 """
 
+
 class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        
+
     def __str__(self):
         return f'{self.name}'
-    
+
     def item_description(self):
         return f'{self.description}'
+
+    def on_take(self):
+        print(f'You have picked up {self.name}')
